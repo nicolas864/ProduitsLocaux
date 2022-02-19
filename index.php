1fbb372d2f24d0316920fd9bf84f2520e4c32a3b
nicolas864 <50581972+nicolas864@users.noreply.github.com>
2022-02-19 17:45:55 +0100

    first commit

---
+++ index.php
@@ -0,0 +1,13 @@
+<!doctype html>
+<html lang="fr">
+<head>
+  <meta charset="utf-8">
+  <title>Les produits locaux</title>
+  <link rel="stylesheet" href="../css/style.css">
+</head>
+<body>
+  <header>
+    <img src="images/plaine.jpg">
+  </header>
+</body>
+</html>
